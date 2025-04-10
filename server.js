10e6a0dcb60b75760e33f9383f9b192f88b8e07a
Krithck Kishore T <krithckkishore.t@gmail.com>
2025-04-10 11:36:56 +0530

    file upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express =require('express');
+const app = express();
+
+const nodemailer = require("nodemailer");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname + 'public/index.html')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user:'krithckkishoret@gmail.com',
+            pass: 'pigo dgcy wqnc srbo'
+        }
+    })
+
+    const mailoptions = {
+        from:req.body.email,
+        to: 'krithckkishoret@gmail.com',
+        text: req.body.message
+
+    }
+
+    transporter.sendMail(mailoptions,(error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('email sent: '+info.response);
+            res.send('success')
+        }
+    })
+
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
