ea5838622ebc3c7a8a5a1fe7042403fa21d63041
Krithck Kishore T <krithckkishore.t@gmail.com>
2025-04-11 22:25:44 +0530

    rr

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: 'https://portfolio-ohp1.onrender.com'
+}));
 
 const nodemailer = require("nodemailer");
 
@@ -12,7 +14,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.use(express.json())
 
-
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log(`Body:`, req.body);
+    next();
+  });
 
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + 'public/index.html')
