31b768724cceb428bb1343ee0dbd549ad86f83d3
Krithck Kishore T <krithckkishore.t@gmail.com>
2025-04-11 14:12:24 +0530

    nnn

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const video1 = document.getElementById('projectvideo1');
 const video2 = document.getElementById('projectvideo2');
+const video3 = document.getElementById('projectvideo3');
 const hoversign = document.querySelector(".hover-sign");
 
 const sideBar = document.querySelector('.sidebar');
@@ -12,7 +13,7 @@ let email = document.getElementById('email');
 let message = document.getElementById('message');
 
 
-const videolist = [video1, video2];
+const videolist = [video1, video2, video3];
 
 videolist.forEach(function(video){
     video.addEventListener('mouseover',function(){
